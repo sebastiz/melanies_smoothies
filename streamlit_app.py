f91c2046ef321baa440ed1b1b7ac952199c48769
Gastroenterology Data Science <sebastiz@users.noreply.github.com>
2025-12-30 10:50:51 +0000

    Update smoothie API response handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!')
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+sf_df=dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
