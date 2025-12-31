ea07344e6ee6c14aac9857361c5156cabe60b878
Gastroenterology Data Science <sebastiz@users.noreply.github.com>
2025-12-31 11:14:06 +0000

    Import pandas and convert DataFrame to pandas format
    Added pandas import and converted Snowflake DataFrame to pandas DataFrame for better handling.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
+
 from snowflake.snowpark.functions import col
 import requests
 
@@ -20,8 +22,12 @@ st.write("The name on your smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+pd_df=my_dataframe.to_pandas()
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
+
 ingredients_list= st.multiselect("Choose up to 5 ingredients:",
                                my_dataframe
                                , max_selections=5
