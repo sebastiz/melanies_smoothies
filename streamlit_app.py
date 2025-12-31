f83ea63bdf924d50adbbed750aab6850a3135bf1
Gastroenterology Data Science <sebastiz@users.noreply.github.com>
2025-12-31 09:17:35 +0000

    Refactor ingredient display logic in streamlit_app
    Removed redundant display of ingredients list and formatted the ingredients string.

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,12 +28,10 @@ ingredients_list= st.multiselect("Choose up to 5 ingredients:",
                                )
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
     ingredients_string=''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
+        ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutritional information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
